5f427f40d9ea7b2c6172a881bfd636e1bfd2d58b
Ismael045 <adamismael297@gmail.com>
2023-06-16 05:35:25 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-
 /**
  * main - main block
  * Return: 1
@@ -11,7 +10,6 @@ int main(void)
 	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
 
 	write(2,
-	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
-	      s);
+	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
