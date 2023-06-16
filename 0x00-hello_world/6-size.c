ed47913e99b49101e9fc78da18e7d32c17b4e3d2
Ismael045 <adamismael297@gmail.com>
2023-06-16 05:32:43 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-
 /**
- * main - Entry point to get the value types
- * Return 0 (success)
+ * main - main block
+ * Return: 0
  */
 int main(void)
 {
-	printf("size of char: %li byte(s)\n", sizeof(char));
-	printf("size of an int: %li byte(s)\n", sizeof(int));
-	printf("size of a long int: %li byte(s)\n", sizeof(long int));
-	printf("size of a long long int: %li byte(s)\n", sizeof(long long int));
-	printf("size of a float: %li byte(s)\n", sizeof(float));
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
 	return (0);
 }
