0b370e7db3da9be81c3c500d84ece6445c61e784
Ismael045 <adamismael297@gmail.com>
2023-06-20 06:38:58 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Print alpahbet: Print alphabet in lowercase using only _putchar
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+
+		_putchar('n');
+		n++;
+	}
+	_putchar('\n');
+}
+
