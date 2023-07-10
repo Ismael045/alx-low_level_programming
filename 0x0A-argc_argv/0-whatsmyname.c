3ef72754dd196a5623f6092c6d0d8b6a28f2ece3
Ismael045 <adamismael297@gmail.com>
2023-07-10 02:35:42 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,11 +3,10 @@
 
 /**
  * main - print the name of the file followed by a new line
- * @argc: number of arguments
- * @argv: array of arguments
- * Return : Always 0 (success)
+ * @argc: argument count
+ * @argv: arguments passed to main as an array of strings
+ * Return: 0
  */
-
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
