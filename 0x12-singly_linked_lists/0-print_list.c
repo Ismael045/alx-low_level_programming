f34185af379b8afb529cd18b2c60fd064fd87a42
Ismael045 <adamismael297@gmail.com>
2023-10-19 03:00:52 -0700

    elements

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * print_list - prints all elements of a list_t list.
+ * @h: singly linked list.
+ * Return: The number of elements.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t elements;
+
+	elements = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		elements++;
+	}
+	return (elements);
+}
+
+
+
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+0x12. C - Singly linked lists
+
+A Linked List is a linear data structure used for storing a collection of elements. Unlike arrays, linked lists use nodes to store elements which are not stored in contiguous memory locations. 
