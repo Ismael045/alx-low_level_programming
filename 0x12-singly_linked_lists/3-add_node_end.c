2fc4f09e097f29ed1ac1938d49b681e330bc2a5b
Ismael045 <adamismael297@gmail.com>
2023-10-19 10:34:52 -0700

    new node

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -8,7 +8,7 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *newnode;
+	list_t *newnode, *tmp;
 	unsigned int i, count = 0;
 
 	newnode = malloc(sizeof(list_t));
@@ -18,8 +18,17 @@ list_t *add_node_end(list_t **head, const char *str)
 	for (i = 0; str[i] != '\0'; i++)
 		count++;
 	newnode->len = count;
-	newnode->next = *head;
-	*head = newnode;
+	newnode->next = NULL;
+	tmp = *head;
 
+	if (tmp == NULL)
+		*head = newnode;
+	else
+	{
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+
+		tmp->next = newnode;
+	}
 	return (*head);
 }
